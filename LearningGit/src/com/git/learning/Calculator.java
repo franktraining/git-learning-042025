e97fa676de0c9d9c8bb1629b2c8010f6721d41a7
franktraining <musabaytechcorp@gmail.com>
2025-04-05 14:19:08 -0400

    fixed divid method, missing ";" and missing return statement.

--- LearningGit/src/com/git/learning/Calculator.java
+++ LearningGit/src/com/git/learning/Calculator.java
@@ -1,28 +1,29 @@
 package com.git.learning;
 
 public class Calculator {
-	
+
 	public int add(int x, int y) {
 		int result = 0;
-		result = x + y; 
+		result = x + y;
 		return result;
 	}
 
 	public int multiply(int x, int y) {
 		return x * y;
 	}
-	
+
 	public int divid(int x, int y) {
-		int result = 0
-				
-		if(y != 0) {
-			result = x / y;
-		}else {
-			throw new Exception(" you cannot divide number by Zero.");
+		int result = 0;
+		try {
+			if (y != 0) {
+				result = x / y;
+			} else {
+				throw new Exception(" you cannot divide number by Zero.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
-		
+		return result;
 	}
-	
-	
+
 }
